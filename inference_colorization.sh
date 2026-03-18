70f61ed19e62f2180513c4d5f63cde796809afdf
Jingjia Feng <jingjiafeng9@gmail.com>
2026-03-18 10:59:47 -0400

    Refactor inference script for colorization parameters

--- inference_colorization.sh
+++ inference_colorization.sh
@@ -1,2 +1,6 @@
-python inference_colorization.py -i /data1/hp/celeba_512_validation_color_lq/ -o /data1/hp/results/cvpr25/cf_scoreinject/celeblq_grayinput_s9_w0 -ckpt /home/hp/work/CodeFormer_aesthetic/experiments/20241104_165750_CodeFormer_colorization/models/net_g_200000.pth -score 0.95
+python inference_colorization.py \
+    -i inputs/cropped_faces \
+    -o results/colorization \
+    -ckpt weights/IQPIR_colorization.pth \
+    -score 0.95
 
